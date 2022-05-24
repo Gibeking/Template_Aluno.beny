ccee89f1ad04eac2ddb14e5cf4dcde476bf0de10
Gibeking <99186257+Gibeking@users.noreply.github.com>
2022-05-24 10:31:36 -0300

    done

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/curriculo.jquery/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/curriculo.jquery/script.js
@@ -1,10 +1,20 @@
 alert('OLA,BEM VINDO')
 
+  $(document).ready(function(){
+    $("#flip").click(function(){
+     $("#panel").slideToggle("slow");
+  });
+});
+
+
 $(document).ready(function(){
-    $("#myInput").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#tudo").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myDIV *").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
+});
+
+
+
